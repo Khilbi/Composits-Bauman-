5c3d854ae0139325bd4641f6c8e17710b54baa53
Khilbi <inna14@yandex.ru>
2025-02-16 14:39:08 +0300

    Обновлен файл app

--- preprocessing.py
+++ preprocessing.py
@@ -1,19 +1,19 @@
 import pickle
 import numpy as np
 
-def get_prediction(plotnost, modulupr, amount, epoks, tepm, poverkhplotn, smol, shag, plotnostn, mat, ugol):
+#def get_prediction(plotnost, modulupr, amount, epoks, tepm, poverkhplotn, smol, shag, plotnostn, mat, ugol):
 
-    with open(r'models/best_linearRegression.pkl','rb') as f:
-        model = pickle.load(f)
+    #with open(r'models/best_linearRegression.pkl','rb') as f:
+        #model = pickle.load(f)
 
-    with open(r'models/scaler_x.pkl','rb') as f:
-        scaler_x = pickle.load(f)
+    #with open(r'models/scaler_x.pkl','rb') as f:
+        #scaler_x = pickle.load(f)
 
-    with open(r'models/scaler_y.pkl','rb') as f:
-        scaler_y = pickle.load(f)   
+    #with open(r'models/scaler_y.pkl','rb') as f:
+        #scaler_y = pickle.load(f)   
 
-    params = np.array([plotnost], [modulupr], [amount], [epoks], [tepm], [poverkhplotn], [smol], [shag], [plotnostn], [mat], [ugol]) 
-    params = scaler_x.transform(params)  
-    y_pred = model.predict(params)  
+    #params = np.array([plotnost], [modulupr], [amount], [epoks], [tepm], [poverkhplotn], [smol], [shag], [plotnostn], [mat], [ugol]) 
+    #params = scaler_x.transform(params)  
+    #y_pred = model.predict(params)  
 
-    return scaler_y.inverse_transform([y_pred])
+    #return scaler_y.inverse_transform([y_pred])
